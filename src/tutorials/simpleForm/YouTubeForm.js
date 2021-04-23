43b9a1c4c965fec5888a6eea828d15c537ae5262
mihaiblandu <mihaiblandu@icloud.com>
2021-04-23 16:24:00 +0300

    Add formik

--- src/tutorials/simpleForm/YouTubeForm.js
+++ src/tutorials/simpleForm/YouTubeForm.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import "./Form.css"
+import {useFormik} from 'formik'
 
 export default function YouTubeForm() {
+
+     const formik = useFormik({
+         
+     })
     return (
         <div align="center">
             <form >
             <label htmlFor='name'>Name</label>
-            <input name='name' id='name' placeholder="the name of the video" type='text'></input>
-            
+            <input name='name' id='name' placeholder="the name of the video" type='text'></input>  
             <label htmlFor='email'>email</label>
             <input name='email' id='email' placeholder="the email of the video" type='text'></input>
             <label htmlFor='Channel'>Channel</label>
